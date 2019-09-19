eb77fa00ff8d23f572f1b84cbda9e35864490107
Nabeel khan <nabeelkhan@qainfotech.com>
2019-09-19 12:45:11 +0530

    notification feature

--- src/Modules/Dashboard/reducer.js
+++ src/Modules/Dashboard/reducer.js
@@ -1,13 +1,18 @@
 import { USER } from "./action";
 const initialState = {
-    leadsData : []
+    leadsData : [],
+    Notification : []
 }
 export function dashboardReducer(state = initialState,action) {
     switch(action.type) {
         case USER.LEADS_DATA :
             state={...state,leadsData:action.payload.data }
-            console.log(state,'reducer');
             return state;
+        case USER.NOTIFICATION :
+            state={...state,Notification:action.payload.data}
+            return state;
+        case USER.POST_NOTIFICATION : 
+        state={...state,Notification: action.payload.data}
         default:
             return state;
     }
