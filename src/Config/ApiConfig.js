463221d0a2bf2721b21ea3cb6c0c25fe8f5ce4e1
Tapan Kumar <tapankumar@QAIT.COM>
2019-09-25 18:05:11 +0530

    Updated API base url to point to deployed env

--- src/Config/ApiConfig.js
+++ src/Config/ApiConfig.js
@@ -1,5 +1,5 @@
 export default {
-  API_BASE_URL: "http://10.0.17.180/api/v1/",
+  API_BASE_URL: "https://api.prometheusschool.com/api/v1/",
   CLIENT_ID: 2,
   CLIENT_SECRET: "lCMBZyVNcQq2nhJ2pHz7zxxbJ0TenqhMceox8fZ7", //"wj9clx4iZtMbenoC6c8tSLmV08FzkxxujwK39ArU",
   ENDPOINTS: {
