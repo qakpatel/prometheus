166f9f89ef07098dd8343e9db255fe70076c1b3b
Abhijit Srivastava <abhijitsrivastava@qainfotech.com>
2019-09-25 19:04:25 +0530

    bug fixes for timeline.js

--- src/Modules/LeadManagment/Dialog/api/dialogservice.js
+++ src/Modules/LeadManagment/Dialog/api/dialogservice.js
@@ -1,9 +1,9 @@
 import ApiClient from "../../../../Utility/ApiClient";
 import ApiConfig from "../../../../Config/ApiConfig";
-let token=JSON.parse(localStorage.getItem('USER'))
+
 const DialogService = {
 	getDialogData: (lead_id) => {
-		
+		let token=JSON.parse(localStorage.getItem('USER'))
 		return new Promise((resolve, reject) => {
 			ApiClient.executeRequest(`leads/${lead_id}/tasks/create`, {}, {}, ApiConfig.METHODS.GET,token.access_token).then(function(response) {
 			    console.log('dialog data',response)
@@ -18,6 +18,7 @@ const DialogService = {
 		})
 	},
 	submitDialog:(data)=>{
+		let token=JSON.parse(localStorage.getItem('USER'))
 		return new Promise((resolve, reject) => {
 			ApiClient.executeRequest(`leads/${data.lead_id}/tasks`, {}, data, ApiConfig.METHODS.POST,token.access_token).then(function(response) {
 			    console.log('dialog data',response)
